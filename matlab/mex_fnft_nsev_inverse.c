963348ea67b9c77c25e49955149ab82c75a4fd58
Sander Wahls <s.wahls@tudelft.nl>
2018-07-31 21:08:20 +0200

    Fixed in mex_fnft_nsev_inverse: dstype_residues option was setting cstype

--- matlab/mex_fnft_nsev_inverse.c
+++ matlab/mex_fnft_nsev_inverse.c
@@ -119,7 +119,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
         } else if ( strcmp(str, "dstype_residues") == 0 ) {
 
-            opts.contspec_type = fnft_nsev_inverse_dstype_RESIDUES;
+            opts.discspec_type = fnft_nsev_inverse_dstype_RESIDUES;
 
         } else if ( strcmp(str, "csmethod_tfmatrix_contains_refl_coeff") == 0 ){
 
