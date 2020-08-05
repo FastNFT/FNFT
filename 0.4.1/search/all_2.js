433b8a1066e33fff31a9438c2367902c944ea39c
Sander Wahls <s.wahls@tudelft.nl>
2020-08-05 14:08:51 +0200

    added docs for v0.4.1

---
+++ 0.4.1/search/all_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['data_20types_5',['Data types',['../group__data__types.html',1,'']]],
+  ['discretization_6',['discretization',['../structfnft__kdvv__opts__t.html#a873649e084067d1b3d5c5a2c9a1bc989',1,'fnft_kdvv_opts_t::discretization()'],['../structfnft__nsep__opts__t.html#ace01aecde86646ac9c35e67e366ed04f',1,'fnft_nsep_opts_t::discretization()'],['../structfnft__nsev__opts__t.html#a04d56679e23081e3c23b4418529ad76a',1,'fnft_nsev_opts_t::discretization()'],['../structfnft__nsev__inverse__opts__t.html#af16b1ac1aa0fb30047ff979b646203ba',1,'fnft_nsev_inverse_opts_t::discretization()']]],
+  ['discspec_5ftype_7',['discspec_type',['../structfnft__nsev__opts__t.html#a55744abb342fa21f0130dd201aa00364',1,'fnft_nsev_opts_t::discspec_type()'],['../structfnft__nsev__inverse__opts__t.html#aa974a824c95a0fb4881c5fed9d402d60',1,'fnft_nsev_inverse_opts_t::discspec_type()']]],
+  ['dsub_8',['Dsub',['../structfnft__nsep__opts__t.html#a15e7ceda36e04ad3ba53d8714bcf3807',1,'fnft_nsep_opts_t::Dsub()'],['../structfnft__nsev__opts__t.html#a37e1524892b31fc3ec5754d9765297b8',1,'fnft_nsev_opts_t::Dsub()']]]
+];
