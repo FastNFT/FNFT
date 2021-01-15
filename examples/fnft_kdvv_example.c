02a0b324fcf73572371f7cadbc39217c4cb23dac
Peter J. Prins <p.j.prins@tudelft.nl>
2021-01-15 17:27:51 +0100

    Update C example KdV

--- examples/fnft_kdvv_example.c
+++ examples/fnft_kdvv_example.c
@@ -15,6 +15,7 @@
  *
  * Contributors:
  * Sander Wahls (TU Delft) 2018.
+ * Peter J. Prins (TU Delft) 2021.
  */
 
 // This example demonstrates the use of the function fnft_kdvv, which 
@@ -59,15 +60,28 @@ int main()
     // Buffer for result: Samples of the continuous spectrum
     FNFT_COMPLEX contspec[M];
 
+    // Maximum number of bound states we expect = size of the two arrays below
+    FNFT_UINT K = D;
+
+    // Buffer for result: Bound states
+    FNFT_COMPLEX bound_states[K];
+
+    // Buffer for result: Norming constants
+    FNFT_COMPLEX normconsts[K];
+
     // Default options
     fnft_kdvv_opts_t opts = fnft_kdvv_default_opts();
+
+    // Uncomment the next line to compute residues instead of norming constants
+    //opts.discspec_type = fnft_kdvv_dstype_RESIDUES;
 
     // See the header file fnft_kdvv.h for the options
 
     /** Step 3: Call fnft_nsev and check for errors **/
 
-    int ret_code = fnft_kdvv(D, q, T, M, contspec, XI, NULL, NULL, NULL,
-        &opts);
+    int ret_code = fnft_kdvv(D, q, T, M, contspec, XI, &K, bound_states,
+                             normconsts, &opts);
+
     if (ret_code != FNFT_SUCCESS) {
         printf("An error occured!\n");
         return EXIT_FAILURE;
@@ -85,6 +99,16 @@ int main()
             (double)xi,
             (double)FNFT_CREAL(contspec[i]),
             (double)FNFT_CIMAG(contspec[i])
+        );
+    }
+
+    printf("Discrete spectrum:\n");
+    for (FNFT_UINT i=0; i<K; i++) {
+        printf("  bound state at %g + %gI with norming constant %g + %gI\n",
+            (double)FNFT_CREAL(bound_states[i]),
+            (double)FNFT_CIMAG(bound_states[i]),
+            (double)FNFT_CREAL(normconsts[i]),
+            (double)FNFT_CIMAG(normconsts[i])
         );
     }
 
