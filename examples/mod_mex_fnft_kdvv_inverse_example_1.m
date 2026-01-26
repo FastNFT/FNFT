05e4c2dea41668893c8a13b24525db0448f09b25
FF <FF>
2026-01-26 12:49:24 +0100

    first working version with early stage of crum transformation feature. One test case is hard implemented yet.

--- examples/mod_mex_fnft_kdvv_inverse_example_1.m
+++ examples/mod_mex_fnft_kdvv_inverse_example_1.m
@@ -15,16 +15,17 @@
 % Contributors:
 % Sander Wahls (KIT) 2026.
 
-clear all;
 close all;
 
 contspec = [];
 XI = [0 1];
-bound_states = 1i*[1, 2, 3, 4] /2;
-normconsts = complex([-1, 1, -1, 1].*[10, 0.1, 1, 0.00001]);
+% bound_states = 1i*[1, 2, 3, 4] /2;
+bound_states = 1i*[1] /2;
+% normconsts = complex([-1, 1, -1, 1].*[10, 0.1, 1, 0.00001]);
+normconsts = complex([1].*[10]);
 D = 1001;
 T = [-20 20];
-q = mex_fnft_kdvv_inverse(contspec, XI, bound_states, normconsts, D, T)
+q = mex_fnft_kdvv_inverse(contspec, XI, bound_states, normconsts, D, T);
 
 t = linspace(T(1), T(2), D);
 plot(t, q)
--- src/fnft_kdvv_inverse.c
+++ src/fnft_kdvv_inverse.c
@@ -20,6 +20,246 @@
 #define FNFT_ENABLE_SHORT_NAMES
 
 #include "fnft_kdvv_inverse.h"
+#include <stdio.h> // for printf
+
+
+// INT input_multisoliton_E_pairs(
+//     UINT const K,
+//     COMPLEX const * const bound_states,
+//     COMPLEX const * const normconsts_or_residues,
+//     const UINT D,
+//     COMPLEX * const q,
+//     REAL const * const T)
+// {
+//     INT ret_code = SUCCESS;
+
+//     // Initialize local variables
+//     COMPLEX tmp;
+//     COMPLEX * bnd_states = NULL;
+//     COMPLEX * norm_consts = NULL;
+
+//     bnd_states = malloc(K * sizeof(COMPLEX));
+//     CHECK_NOMEM(bnd_states, ret_code, leave_fun);
+//     norm_consts = malloc(K * sizeof(COMPLEX));
+//     CHECK_NOMEM(norm_consts, ret_code, leave_fun);
+
+//     //sorting bnd_states and according norm_const in descending order based on magnitude of imaginary part
+//     for (UINT i = 0; i < K; ++i){
+//         for (UINT j = i + 1; j < K; ++j){
+//             if (CIMAG(bnd_states[i]) < CIMAG(bnd_states[j])){
+//                 tmp =  bnd_states[i];
+//                 bnd_states[i] = bnd_states[j];
+//                 bnd_states[j] = tmp;
+//                 tmp =  norm_consts[i];
+//                 norm_consts[i] = norm_consts[j];
+//                 norm_consts[j] = tmp;
+//             }
+//         }
+//     }
+
+    
+
+//     // Apply normconst
+//     //TODO
+
+//     // Classic Crum transformation
+
+//     UINT N_rest = K;
+    
+//     while(N_rest > 0){
+
+//         // Scale trajectories
+//         //TODO  
+
+//         ret_code = add_two_solitons_E(q, x_grid, );
+//         CHECK_RETCODE(ret_code, leave_fun);
+//     }
+
+// leave_fun:
+//     free(bound_states);
+//     free(norm_consts);
+
+//     return ret_code;
+// }
+
+
+INT add_one_soliton_E(
+    COMPLEX const * const bound_state,
+    REAL const * const theta_E1,
+    REAL const * const theta_E2,
+    REAL const * const x_grid,
+    const UINT D,
+    COMPLEX * const q,
+    REAL const * const T)
+{
+    INT ret_code = SUCCESS;
+
+    COMPLEX * M_min1_11 = NULL;
+    M_min1_11 = malloc(D * sizeof(COMPLEX));
+    CHECK_NOMEM(M_min1_11, ret_code, leave_fun);
+
+    //TODO: I weg!!
+    COMPLEX k1 = bound_state[0]*I;
+
+    // printf("Marker1");
+
+    // Determine where x>0
+    UINT i_pos;
+    
+    for (UINT i = 0; i < D; i++){
+        if (x_grid[i] > 0){
+            i_pos = i;
+            break;
+        }
+    }
+
+    UINT n_pos=D-i_pos;
+    UINT n_neg=i_pos;
+
+    // Divide x_grid into positive and negative
+    REAL * x_pos = NULL;
+    REAL * x_neg = NULL;
+
+    x_pos = malloc(n_pos * sizeof(REAL));
+    CHECK_NOMEM(x_pos, ret_code, leave_fun);
+
+    x_neg = malloc(n_neg * sizeof(REAL));
+    CHECK_NOMEM(x_neg, ret_code, leave_fun);
+
+    for (UINT i = 0; i < D; i++){
+        if (i < i_pos){
+            x_neg[i] = x_grid[i];
+        } else {
+            x_pos[i - i_pos] = x_grid[i];
+        }
+    }
+
+    // divide theta_E
+    COMPLEX * th1_pos = NULL;
+    COMPLEX * th2_pos = NULL;
+    COMPLEX * th1_neg = NULL;
+    COMPLEX * th2_neg = NULL;
+
+    th1_pos = malloc(n_pos * sizeof(COMPLEX));
+    CHECK_NOMEM(th1_pos, ret_code, leave_fun);
+    th2_pos = malloc(n_pos * sizeof(COMPLEX));
+    CHECK_NOMEM(th2_pos, ret_code, leave_fun);
+    th1_neg = malloc(n_neg * sizeof(COMPLEX));
+    CHECK_NOMEM(th1_neg, ret_code, leave_fun);
+    th2_neg = malloc(n_neg * sizeof(COMPLEX));
+    CHECK_NOMEM(th2_neg, ret_code, leave_fun);
+
+    for (UINT i=0; i<n_neg; i++){
+        th1_neg[i] = theta_E1[i];
+        th2_neg[i] = theta_E2[i];
+    }
+
+    for (UINT i=0; i<n_pos; i++){
+        th1_pos[i] = theta_E1[i + i_pos];
+        th2_pos[i] = theta_E2[i + i_pos];
+    }
+
+    // Calculation for positive x
+    
+    COMPLEX * w_p_inv = NULL;
+    w_p_inv = malloc(n_pos * sizeof(COMPLEX));
+    CHECK_NOMEM(w_p_inv, ret_code, leave_fun);
+
+    for (UINT i=0; i<n_pos; i++){
+        w_p_inv[i] = 1/(th1_pos[i] + th2_pos[i]*CEXP(2*k1*x_pos[i]));
+    }
+
+    COMPLEX * prefactor_pos = NULL;
+    prefactor_pos = malloc(n_pos * sizeof(COMPLEX));
+    CHECK_NOMEM(prefactor_pos, ret_code, leave_fun);
+
+    for (UINT i=0; i<n_pos; i++){
+        prefactor_pos[i] = 2*k1*k1*th1_pos[i]*th2_pos[i]*w_p_inv[i]*w_p_inv[i];
+    }
+
+    for (UINT i=0; i<n_pos; i++){
+        M_min1_11[i+n_neg]=prefactor_pos[i]*CEXP(2*k1*x_pos[i]);
+    }
+    
+
+    // Calculation for negative x
+
+    COMPLEX * w_n_inv = NULL;
+    w_n_inv = malloc(n_neg * sizeof(COMPLEX));
+    CHECK_NOMEM(w_n_inv, ret_code, leave_fun);
+
+    for (UINT i=0; i<n_neg; i++){
+        w_n_inv[i] = 1/(th1_neg[i]*CEXP(-2*k1*x_neg[i]) + th2_neg[i]);
+    }
+
+    COMPLEX * prefactor_neg = NULL;
+    prefactor_neg = malloc(n_neg * sizeof(COMPLEX));
+    CHECK_NOMEM(prefactor_neg, ret_code, leave_fun);
+
+    for (UINT i=0; i<n_neg; i++){
+        prefactor_neg[i] = 2*k1*k1*th1_neg[i]*th2_neg[i]*w_n_inv[i]*w_n_inv[i];
+    }
+
+    for (UINT i=0; i<n_neg; i++){
+        M_min1_11[i] = prefactor_neg[i]*CEXP(-2*k1*x_neg[i]);
+    }
+
+    // Update output
+    for (UINT i=0; i<D; i++){
+        q[i] = -q[i] + 4 * M_min1_11[i];
+        // q[i] = x_grid[i];
+        // q[i] = M_min1_11[i];
+
+        // if (i<n_neg){
+        //     // q[i] = prefactor_neg[i];
+        //     // q[i] = w_n_inv[i];
+        //     // q[i] = th1_neg[i] + th2_neg[i] + x_neg[i];
+        //     q[i] = th1_neg[i];
+        // } else {
+        //     // q[i] = prefactor_pos[i-n_neg];
+        //     // q[i] = w_p_inv[i-n_neg];
+        //     // q[i] = th1_pos[i-i_pos] + th2_pos[i-i_pos] + x_pos[i-i_pos];
+        //     q[i] = th1_pos[i-i_pos];
+        // }
+
+        // if (i<n_neg){
+        //     q[i] = CEXP(-2*k1*x_neg[i]);
+        // } else {
+        //     q[i] = CEXP(2*k1*x_pos[i-n_neg]);
+        // }
+        
+    }
+
+leave_fun:
+    free(x_pos);
+    free(x_neg);
+    free(th1_pos);
+    free(th2_pos);
+    free(th1_neg);
+    free(th2_neg);
+    free(w_p_inv);
+    free(prefactor_pos);
+    free(w_n_inv);
+    free(prefactor_neg);
+
+    return ret_code;
+}
+
+
+// INT add_two_solitons_E(
+//     UINT const K,
+//     COMPLEX const * const bound_states,
+//     COMPLEX const * const normconsts_or_residues,
+//     const UINT D,
+//     COMPLEX * const q,
+//     REAL const * const T)
+// {
+//     COMPLEX k1;
+//     COMPLEX k2;
+// }
+
+
+
 INT fnft_kdvv_inverse(
     const UINT M,
     COMPLEX * const contspec,
@@ -32,8 +272,66 @@ INT fnft_kdvv_inverse(
     REAL const * const T,
     void *opts_ptr)
 {
-    for (UINT n=0; n<D; n++)
+    
+    /*
+    D       number of spatial points
+    out     output of transformation
+
+    */
+
+    /* Tests
+    number of normconsts = number bound_states
+    */
+
+   
+    INT ret_code = SUCCESS;
+
+    // Initialize q
+
+    for (UINT n=0; n<D; n++){
         q[n] = 0;
-    return SUCCESS;
+    }
+
+    // Initialize spatial grid with D points between T[0] and T[1]
+    REAL * x_grid = NULL;
+    x_grid = malloc(D * sizeof(REAL));
+    CHECK_NOMEM(x_grid, ret_code, leave_fun);
+
+    const REAL eps_t = (T[1] - T[0])/(D - 1);
+
+    for (UINT n=0; n<D; n++) {
+        x_grid[n]= T[0] + n*eps_t;
+    }
+
+    // Initialize theta_E
+
+    REAL * theta_E1 = NULL;
+    theta_E1 = malloc(D * sizeof(REAL));
+    CHECK_NOMEM(theta_E1, ret_code, leave_fun);
+
+    REAL * theta_E2 = NULL;
+    theta_E2 = malloc(D * sizeof(REAL));
+    CHECK_NOMEM(theta_E2, ret_code, leave_fun);
+
+    for (UINT i=0; i<D; i++){
+        theta_E1[i]=1;
+        theta_E2[i]=10;
+    }
+
+    // Add solitions
+
+    for (UINT i=0; i<K; i++){
+        ret_code = add_one_soliton_E(bound_states, theta_E1, theta_E2, x_grid, D, q, T);
+    }
+
+    // for (UINT n=0; n<D; n++){
+    //     q[n] = x_grid[n];
+    // }
+        
+
+leave_fun:
+    free(x_grid);
+
+    return ret_code;
 }
 
