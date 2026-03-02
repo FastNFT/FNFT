2ec4c170fed39c6773587a4bde5a8802ccd80c7d
FF <FF>
2026-03-02 10:16:49 +0100

    Add struct fnft_kdvv_params and implement it into functions  in fnft_kdvv_inverse_test_1

--- test/fnft_kdvv_inverse/fnft_kdvv_inverse_test_1.c
+++ test/fnft_kdvv_inverse/fnft_kdvv_inverse_test_1.c
@@ -24,6 +24,19 @@
 #include "fnft__errwarn.h"
 #include "fnft__misc.h"
 
+
+typedef struct {
+    UINT D;
+    REAL T[2];
+    UINT K;
+    COMPLEX * bound_states;
+    COMPLEX * normconsts;
+    UINT M;
+    REAL XI[2];
+    REAL * contspec;
+} fnft_kdvv_params;
+
+
 static INT print_test_results(  COMPLEX * bound_states_r,
                                 COMPLEX * normconsts_r,
                                 COMPLEX * contspec_r,
@@ -58,44 +71,36 @@ static INT print_test_results(  COMPLEX * bound_states_r,
 
 
 
-static INT run_test(const UINT D,
+static INT run_test(const fnft_kdvv_params params_i,
                     REAL err_bnd_bound_states,
                     REAL err_bnd_spurious_bound_states,
                     REAL err_bnd_normconst,
                     REAL err_bnd_contspec)
 
 {
-INT ret_code = SUCCESS;
-    
-    // General parameters
-    UINT M = 10;
-    REAL XI[2] = {-2.0, 2.0};
-    REAL T[2] = {-20.0, 20.0};
+    INT ret_code = SUCCESS;
 
     COMPLEX * q = NULL;
-    q = malloc(D * sizeof(COMPLEX));
+    q = malloc(params_i.D * sizeof(COMPLEX));
     CHECK_NOMEM(q, ret_code, leave_fun);
 
-    // Initialize variables for a specific test case without continuous spectrum
-    COMPLEX * contspec_i = NULL;
-    contspec_i = malloc(M * sizeof(COMPLEX));
-    const UINT K_i = 5;
-    COMPLEX bound_states_i[5] = {I*SQRT(1.0/2.0), I*SQRT(2.0/2.0), I*SQRT(3.0/2.0), I*SQRT(4.0/2.0), I*SQRT(5.0/2.0)};
-    COMPLEX normconsts_i[5] = {1*10, -1*0.1, 1*1, -1*1e-5, 1*1e7};
-
-    // const UINT K_i = 1;
-    // COMPLEX bound_states_i[1] = {SQRT(1.0/2)*I};
-    // COMPLEX normconsts_i[1] = {1*10};
-
-    ret_code = fnft_kdvv_inverse(M, contspec_i, XI, K_i, bound_states_i, normconsts_i, D, q, T, NULL);
+    ret_code = fnft_kdvv_inverse(   params_i.M, 
+                                    params_i.contspec, 
+                                    params_i.XI, 
+                                    params_i.K, 
+                                    params_i.bound_states, 
+                                    params_i.normconsts, 
+                                    params_i.D, 
+                                    q, 
+                                    params_i.T, NULL);
     CHECK_RETCODE(ret_code, leave_fun);
     
     // Prepare forward fnft_kdvv
     COMPLEX * contspec_r = NULL;
-    contspec_r = malloc(M * sizeof(COMPLEX));
+    contspec_r = malloc(params_i.M * sizeof(COMPLEX));
     CHECK_NOMEM(contspec_r, ret_code, leave_fun);
 
-    UINT K_r = D;
+    UINT K_r = params_i.D;
 
     COMPLEX * bound_states_r = NULL;
     bound_states_r = malloc(K_r * sizeof(COMPLEX));
@@ -107,15 +112,15 @@ INT ret_code = SUCCESS;
 
     fnft_kdvv_opts_t opts = fnft_kdvv_default_opts();
 
-    ret_code = fnft_kdvv(D, q, T, M, contspec_r, XI, &K_r, bound_states_r, normconsts_r, &opts);
+    ret_code = fnft_kdvv(params_i.D, q, params_i.T, params_i.M, contspec_r, params_i.XI, &K_r, bound_states_r, normconsts_r, &opts);
     CHECK_RETCODE(ret_code, leave_fun);
 
     // -- Check results --
 
     // Simple general tests
-    for (UINT i=0; i<K_i; i++){
-        COMPLEX bsi = bound_states_i[i];   
-        COMPLEX nci = normconsts_i[i];
+    for (UINT i=0; i<params_i.K; i++){
+        COMPLEX bsi = params_i.bound_states[i];   
+        COMPLEX nci = params_i.normconsts[i];
         COMPLEX bsr = bound_states_r[i];
         COMPLEX ncr = normconsts_r[i];
 
@@ -139,27 +144,27 @@ INT ret_code = SUCCESS;
 
     // Check matching of the computed  bound states
     // bound_states_r is sorted in ascending order, K_r is the number of found bound states by fnft_kdvv
-    // take only the last K_i bound states, because they are the bigger ones and more likely the ones
+    // take only the last params_i.K bound states, because they are the bigger ones and more likely the ones
     // which corresponds to the initial given bound states
-    COMPLEX * const candidate_bound_states_r_ptr = &bound_states_r[K_r-K_i];
-    REAL hausdorff_dist_bound_states = misc_hausdorff_dist_normed(  K_i, bound_states_i, K_i, 
+    COMPLEX * const candidate_bound_states_r_ptr = &bound_states_r[K_r-params_i.K];
+    REAL hausdorff_dist_bound_states = misc_hausdorff_dist_normed(  params_i.K, params_i.bound_states, params_i.K, 
                                                                     candidate_bound_states_r_ptr);
     UINT is_bsr_in_tolerance = hausdorff_dist_bound_states < err_bnd_bound_states;                                                                
 
-    COMPLEX * const candidate_normconsts_r_ptr = &normconsts_r[K_r-K_i];
-    REAL hausdorff_dist_normconsts = misc_hausdorff_dist_normed(K_i, normconsts_i, K_i, 
+    COMPLEX * const candidate_normconsts_r_ptr = &normconsts_r[K_r-params_i.K];
+    REAL hausdorff_dist_normconsts = misc_hausdorff_dist_normed(params_i.K, params_i.normconsts, params_i.K, 
                                                                 candidate_normconsts_r_ptr);
     UINT is_ncr_in_tolerance = hausdorff_dist_normconsts < err_bnd_normconst;                                                         
 
     UINT is_contspec_small = 1;                                                                     
-    for (UINT i=0; i<M; i++){
+    for (UINT i=0; i<params_i.M; i++){
         if (CABS(contspec_r[i]) > err_bnd_contspec){
             is_contspec_small = 0;
         }
     }
 
     UINT are_spurious_bound_states_small = 1;
-    for (UINT i=0; i<(K_r-K_i); i++){
+    for (UINT i=0; i<(K_r-params_i.K); i++){
         if (CABS(bound_states_r[i]) > err_bnd_spurious_bound_states){
             are_spurious_bound_states_small = 0;
         }
@@ -186,7 +191,6 @@ INT ret_code = SUCCESS;
 
 leave_fun:
     free(q);
-    free(contspec_i);
     free(contspec_r);
     free(bound_states_r);
     free(normconsts_r);
@@ -201,39 +205,55 @@ leave_fun:
 INT main()
 {
     INT ret_code = SUCCESS;
+
+    COMPLEX bound_states_i[5] = {I*SQRT(1.0/2.0), I*SQRT(2.0/2.0), I*SQRT(3.0/2.0), I*SQRT(4.0/2.0), I*SQRT(5.0/2.0)};
+    COMPLEX normconsts_i[5] = {1*10, -1*0.1, 1*1, -1*1e-5, 1*1e7};
+
+    COMPLEX * contspec_i = NULL;
+
+    fnft_kdvv_params kdvv_parameters = {
+        .D = 256,
+        .T = {-20.0, 20.0},
+        .K = 5,
+        .bound_states = bound_states_i,
+        .normconsts = normconsts_i,
+        .M = 10,
+        .XI = {-2.0, 2.0},
+        .contspec = contspec_i
+    };
+
     
-    UINT D = 256;
     REAL err_bnd_bound_states = 1.4e-3;
     REAL err_bnd_spurious_bound_states = 1e-2;
     REAL err_bnd_normconst = 4.5e-2;
     REAL err_bnd_contspec = 1e-1;
 
     
-    ret_code = run_test(D, err_bnd_bound_states, err_bnd_spurious_bound_states, 
+    ret_code = run_test(kdvv_parameters, err_bnd_bound_states, err_bnd_spurious_bound_states, 
                         err_bnd_normconst, err_bnd_contspec);
 
     CHECK_RETCODE(ret_code, leave_fun);
 
     
     // Check quadratic convergence
-    D *= 2;
+    kdvv_parameters.D *= 2;
     err_bnd_bound_states /= 4;
     err_bnd_spurious_bound_states /= 4;
     err_bnd_normconst = 1.1e-2;
     err_bnd_contspec = 5e-3;
 
-    ret_code = run_test(D, err_bnd_bound_states, err_bnd_spurious_bound_states, 
+    ret_code = run_test(kdvv_parameters, err_bnd_bound_states, err_bnd_spurious_bound_states, 
                         err_bnd_normconst, err_bnd_contspec);
 
     CHECK_RETCODE(ret_code, leave_fun);
 
-    D *= 2;
+    kdvv_parameters.D *= 2;
     err_bnd_bound_states /= 4;
     err_bnd_spurious_bound_states /= 4;
     err_bnd_normconst /= 4;
     err_bnd_contspec /= 4;
 
-    ret_code = run_test(D, err_bnd_bound_states, err_bnd_spurious_bound_states, 
+    ret_code = run_test(kdvv_parameters, err_bnd_bound_states, err_bnd_spurious_bound_states, 
                         err_bnd_normconst, err_bnd_contspec);
 
     CHECK_RETCODE(ret_code, leave_fun);
